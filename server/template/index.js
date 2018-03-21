8c5c8f6f38657edfbf71f4fd7cf8127166999f40
Stanislav Sysoev <d4rkr00t@gmail.com>
2018-03-21 20:05:15 +1100

    feat: add apple touch icon

--- server/template/index.js
+++ server/template/index.js
@@ -43,6 +43,7 @@ function template(record) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="manifest" href="manifest.json">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
   <title>Aircards</title>
   <style>${styles}</style>
 </head>
